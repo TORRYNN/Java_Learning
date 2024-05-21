8d3c1f44e506c0639e5bb19977b9478c4a9a9d56
TORRYNN <prathammchauhann@gmail.com>
2024-05-21 13:00:22 +0530

    Practiced till  Conditional Statement

---
+++ 11_Conditional_Statements/Question3.java
@@ -0,0 +1,37 @@
+
+// Write a Java program to input week number (1-7) and print day of week name using switch case.
+import java.util.*;
+
+public class Question3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednessday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturaday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Wrong Input");
+                break;
+        }
+    }
+}
---
+++ 11_Conditional_Statements/Question5.java
@@ -0,0 +1,20 @@
+// Write a Java program that takes a year from the user and print whether that year is a leap year or not.
+
+import java.util.*;
+
+public class Question5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("leap year");
+                } else {
+                    System.out.println("Not a leap year");
+                }
+            }
+        }
+    }
+}
