46fa1c1cd04b737a94972ef3f598c83c1a1b73d5
TORRYNN <prathammchauhann@gmail.com>
2024-06-05 23:53:52 +0530

    Added some line in the Bubble Sort

--- 18_Basic_Sorting_Algorithm/BubbleSort.java
+++ 18_Basic_Sorting_Algorithm/BubbleSort.java
@@ -1,17 +1,26 @@
+import java.util.*;
 
 public class BubbleSort {
     public static int[] bsort(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
         return arr;
+        // Time Comlexity=O(n2) Space Comlexity=O(1)
+    }
+
+    // This is not a bubble sort sorting
+    public static ArrayList<Integer> bsort(ArrayList<Integer> arr) {
+        Collections.sort(arr);
+        return arr;
+        // Time Comlexity=O(1) Space Comlexity=O(1)
 
     }
 
@@ -25,7 +34,17 @@ public class BubbleSort {
         for (int num : ar) {
             System.out.print(num + " ");
         }
-
+        System.out.println();
+        System.out.println("--------------------------------------------------------------");
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(99, 23, 1, 53, 22, 53));
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        bsort(arr);
+        System.out.println();
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 
 }
