89195e9a45151e1359d743d1c700d0e5c69b61d1
TORRYNN <prathammchauhann@gmail.com>
2024-06-11 18:30:34 +0530

    Implemented Transpose of  Matrix

--- 19_2D_Array/SearchinSortedMatrix.java
+++ 19_2D_Array/SearchinSortedMatrix.java
@@ -35,7 +35,9 @@ public class SearchinSortedMatrix {
 
         while (row < mat.length && col >= 0) {
             if (mat[row][col] == key) {
+                
                 return "key found at index (" + row + "," + col + ")";
+                
             } else if (key < mat[row][col]) {
                 col--;
             } else {
@@ -49,7 +51,7 @@ public class SearchinSortedMatrix {
     public static void main(String[] args) {
         int matrix[][] = { { 1, 2, 3, 4, 5 },
                 { 6, 7, 8, 9, 10 },
-                { 11, 12, 13, 14, 15 },
+                { 18, 12, 18, 14, 15 },
                 { 16, 17, 18, 19, 20 },
                 { 21, 22, 23, 24, 25 } };
         int key = 18;
