0b893803cbb867e404db6d5a4daa27b4809ad6fc
TORRYNN <prathammchauhann@gmail.com>
2024-06-13 22:44:59 +0530

    Completed 2d Arrays

--- 19_2D_Array/Question.java
+++ 19_2D_Array/Question.java
@@ -15,9 +15,20 @@ public class Question {
 
         return count;
     }
+    // Print out the sum of the numbers in the second row of the "nums" array;
+    public static  int sum(int mat[][]){
+        int sum=0;
+        for(int i=0; i<mat.length; i++){
+            sum+=mat[1][i];
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         int[][] array = { { 4, 7, 8 }, { 8, 8, 7 } };
         System.out.println(Count(array, 7)); // Output should be 2
+
+        int[][] nums = { {1,4,9},{11,4,3},{2,2,3} };
+        System.out.println("Sum "+sum(nums));
     }
 }
